be01f429c4253281922efcf8228df0d4180c687e
nikoagge <nikoagge@gmail.com>
2022-04-10 18:24:51 +0300

    Added ColorExtensions

---
+++ ExpenseTracker/Extensions/ColorExtension.swift
@@ -0,0 +1,15 @@
+//
+//  ColorExtension.swift
+//  ExpenseTracker
+//
+//  Created by Nikos Aggelidis on 10/4/22.
+//
+
+import SwiftUI
+
+extension Color {
+    static let backgroundColor = Color("Background")
+    static let iconColor = Color("Icon")
+    static let textColor = Color("Text")
+    static let systemBackgroundColor = Color(uiColor: .systemBackground)
+}
