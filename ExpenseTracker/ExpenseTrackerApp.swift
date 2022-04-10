6104c59df08ec14dbee6228d2ca30f2c3dc8663d
nikoagge <nikoagge@gmail.com>
2022-04-10 18:03:02 +0300

    Initial commit

---
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by Nikos Aggelidis on 10/4/22.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
